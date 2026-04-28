492bb0cee50b9df0696a73f10f87c936212db332
Kateryna <katyaukr2014@gmail.com>
2026-04-28 16:24:46 +0300

    add

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu-button');
+    const iconContainer = menuBtn.querySelector('.icon');
+    const modalMenu = document.querySelector('.modal');
+    const menuItem = document.querySelectorAll('.menu-list-item');
+    const header = document.querySelector('.header');
+  
+    let isMenuOpen = false;
+  
+    const burgerIcon = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect x="0.5" y="0.5" width="23" height="23" rx="11.5" stroke="#D7BDF9" />
+  <path d="M5 7H19M5 17H19M5 12H19" stroke="#444443" stroke-linecap="round" />
+</svg>
+    `;
+  
+    const crossIcon = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect x="0.5" y="0.5" width="23" height="23" rx="11.5" stroke="#D7BDF9" />
+  <path d="M6 6L11.75 11.75M11.75 11.75L6 17.5M11.75 11.75L17.5 17.5M11.75 11.75L17.5 6" stroke="#444443" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
+    `;
+  
+    menuBtn.addEventListener('click', () => {
+      isMenuOpen = !isMenuOpen;
+      iconContainer.innerHTML = isMenuOpen ? crossIcon : burgerIcon;
+      isMenuOpen ? openModal() : closeModal();
+    });
+  
+    menuItem.forEach(item =>
+      item.addEventListener('click', () => {
+        closeModal();
+        iconContainer.innerHTML = burgerIcon;
+        isMenuOpen = false;
+      })
+    );
+  
+    window.addEventListener('scroll', blurHeader);
+  
+    function openModal() {
+      modalMenu.style.display = 'block';
+    }
+  
+    function closeModal() {
+      modalMenu.style.display = 'none';
+    }
+  
+    function blurHeader() {
+      if (!header) return;
+      if (window.scrollY > 60) {
+        header.classList.add('header-scroll');
+      } else {
+        header.classList.remove('header-scroll');
+      }
+    }
+  });
